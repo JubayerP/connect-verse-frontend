118ca1707ea156332695655c4f88aab87c00c62f
JubayerDev <ajashes858@gmail.com>
2023-02-17 20:28:02 +0600

    navbar added

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDN49v-BLEKObJ-QrKegHrpnAUQoeZ7oOo",
+    authDomain: "connect-verse.firebaseapp.com",
+    projectId: "connect-verse",
+    storageBucket: "connect-verse.appspot.com",
+    messagingSenderId: "706886411968",
+    appId: "1:706886411968:web:188136bc9be2696a2e2f25"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
